66335e4c2a859bf77df66931370ec125885b2b55
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-05-24 00:21:59 -0700

    add textarea to Add component

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { Modal } from 'antd';
+import React, { useState } from 'react';
+import { Modal, Select, Input } from 'antd';
+const { Option } = Select;
+const { TextArea } = Input;
 
 const Add = props => {
+  const [question, setQuestion] = useState('question1');
+  const [answer, setAnswer] = useState('');
   return (
     <div>
-      <div>
-        <Modal
-          title="Basic Modal"
-          visible={true}
-          onOk={props.onClick}
-          onCancel={props.onClick}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Modal>
-      </div>
+      <Modal
+        title="Basic Modal"
+        visible={true}
+        onOk={props.onClick}
+        onCancel={props.onClick}
+      >
+        <>
+          <Select
+            defaultValue={question}
+            style={{ width: 120 }}
+            onChange={setQuestion}
+          >
+            <Option value="jack">Jack</Option>
+            <Option value="lucy">Lucy</Option>
+            <Option value="Yiminghe">yiminghe</Option>
+          </Select>
+        </>
+        <TextArea rows={10} onChange={setAnswer} />
+      </Modal>
     </div>
   );
 };
