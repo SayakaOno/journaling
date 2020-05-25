9bc4eb8cae3cdacd9313ba72ecfa4462e080abc1
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-05-24 23:55:04 -0700

    try creating download API

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ var multer = require('multer');
 var cors = require('cors');
 var fs = require('fs');
 
+let fileName = '';
+
 app.use(cors());
+app.use(express.json());
 
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, 'src');
   },
   filename: function(req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    fileName = Date.now() + '-' + file.originalname;
+    cb(null, fileName);
   }
 });
 
@@ -28,6 +32,19 @@ app.post('/upload', function(req, res) {
       return res.status(200).send({ ...req.file, content: data });
     });
   });
+});
+
+app.post('/download', function(req, res) {
+  fileName = '1590363203814-journaling.json';
+  if (!fileName) {
+  // create file
+  }
+  fs.writeFile(`src/${fileName}`, JSON.stringify(req.body.data), function(err) {
+    if (err) throw err;
+    console.log('Updated!', req.body.data);
+  });
+  res.download(`src/${fileName}`);
+  res.data = req.body.data.data;
 });
 
 app.listen(8000, function() {
